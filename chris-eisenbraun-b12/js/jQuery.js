cca7d1099fdf64fcdc71ca41cdc8bdfcaa8b3238
Chris Eisenbraun <hello@chriseisenbraun.com>
2014-03-27 15:32:03 -0700

    Update jQuery file to be 'more awesome'

--- chris-eisenbraun-b12/js/jQuery.js
+++ chris-eisenbraun-b12/js/jQuery.js
@@ -1,10 +1,24 @@
 // Author's jQuery - Chris Eisenbraun
 // Testing creating a variable to be called later.
 var $biosSpan = $("figcaption").find("span");
-// Using variable (non concerned about ugly css injection)
+// Using variable (not concerned about ugly css injection)
 $biosSpan.css({"display": "block", "background": "black", "color": "teal"});
 //Traversing
 $( "h2" ).nextAll("p").css("color", "rgb(63, 177, 248)");
-// Add class AND FLAMES!!!!
+//Examples of DOM manipulation Add class AND FLAMES!!!!
 $( "h1,  h1 a" ).addClass( "why font-effect-fire-animation").css("color", "black");
+// Use of Event Delegation
+$( "a" ).closest( "h4" ) .css( "background-color", "#d4644c" );
+
+
+//Leveraging the assignment from Day I, add more jQuery awesome.
+//
+//Things I am looking for:
+//
+//Examples of DOM manipulation -ok
+//Use of Event Delegation - ok
+//Clear use of vars and the `this` keyword
+//Reading, writing and saving content nodes from the DOM
+//Comparative if statements
+//String concatenation
 
